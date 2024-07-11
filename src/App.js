f14c57f7d4d8d603e9af6eda06fd632ee17def97
SreypichMen <92456785+SreypichMen@users.noreply.github.com>
2024-07-11 23:20:07 +0200

    fix-pull-error

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './assets/styles/App.css';
-import MapPage from './pages /MapView/Map'; 
+import MapPage from './pages/MapView/Map'; 
 import { useTheme } from './contexts/ThemeContext'; 
-import ThemeToggle from './components /ThemeToggle'; 
+import ThemeToggle from './components/ThemeToggle'; 
 function App() {
   const { theme } = useTheme(); 
   useEffect(() => {
