acc817de91a6b10270e0dfbba6c35376ff562727
Sreypich Men <sreypichmen@Sreypichs-Air.lan>
2024-07-11 18:05:00 +0200

    theme-mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css'; // Import the CSS file
-import MapPage from './Map'; // Ensure this path matches your file structure
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './assets/styles/App.css';
+import MapPage from './pages /Map/Map'; // Correct path (remove spaces)
+import { useTheme } from './contexts/ThemeContext'; 
+import ThemeToggle from './components /ThemeToggle'; // Correct path (remove spaces)
 
 function App() {
+  const { theme } = useTheme(); // Use the theme from ThemeContext
+  useEffect(() => {
+    document.body.className = theme === 'dark' ? 'body-dark' : '';
+  }, [theme]); 
+  
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="container">
-              <h1>Home Page</h1>
-              <p>Welcome to the Home Page! Click below to go to the Map Page.</p>
-              <Link to="/map" className="link">Go to Map Page</Link>
+        <Routes>
+          <Route path="/" element={
+            <div  className="App">
+                 <ThemeToggle />
+              <div className="container">
+          
+                <h1>Home Page</h1>
+                <p>Welcome to the Home Page! Click below to go to the Map Page.</p>
+                <Link to="/map" className="link">Go to Map Page</Link>
+              </div>
             </div>
-          }
-        />
-        <Route path="/map" element={<MapPage />} />
-      </Routes>
+          }/>
+          <Route path="/map" element={<MapPage />} />
+        </Routes>
     </Router>
   );
 }
