1fa7fbbd14b7a88ad46891058ee28a083d4a2d64
CYTech Student <sreypichmen364@gmail.com>
2024-07-12 23:12:27 +0200

    update_code_based_comment

---
+++ src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import '../assets/styles/App.css';
+import MapPage from '../pages/MapView/Map';
+import { useTheme } from '../contexts/ThemeContext';
+import ThemeToggle from '../components/ThemeToggle';
+
+function HomePage() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    document.body.className = theme === 'dark' ? 'body-dark' : '';
+  }, [theme]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="App">
+            <ThemeToggle />
+            <div className="container">
+              <h1>Home Page</h1>
+              <p>Welcome to the Home Page! Click below to go to the Map Page.</p>
+              <Link to="/map" className="link">Go to Map Page </Link>
+            </div>
+          </div>
+        }/>
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
-import App from './App'; // This should be the top-level component wrapping everything
-import { ThemeProvider } from './contexts/ThemeContext'; // Ensure this path is correct
+import HomePage from './components/HomePage';
+import { ThemeProvider } from './contexts/ThemeContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <HomePage />
     </ThemeProvider>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
