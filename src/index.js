b53a6fd71b5f8021dbf95adcd16b218844cfd2e5
CYTech Student <sreypichmen364@gmail.com>
2024-07-19 11:29:02 +0200

    fix-error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css'; 
-import HomePage from './pages/HomePage'; 
+import HomePage from './pages/HomePage/HomePage'; 
 import { ThemeProvider } from './contexts/ThemeContext'; 
 import reportWebVitals from './reportWebVitals'; 
 import './i18n';
