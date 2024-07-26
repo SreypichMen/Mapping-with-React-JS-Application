70b9e2363cf6aba74a6bb1508ad124b60904ddf0
Sreypich Men <sreypichmen@Sreypichs-Air.lan>
2024-07-26 16:42:49 +0200

    add-search-place-input

--- src/pages/MapViewPage/MapPage.js
+++ src/pages/MapViewPage/MapPage.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import BackHome from '../../components/MapPage/BackHomeNavigationButton/BackHomeNavigationButton';
 import useCurrentLocation from '../../hooks/useCurrentLocation';
 import { fetchElevationData } from '../../api/elevationService';
+import './MapPage.css';  // Import the CSS file
 
 // Define the custom icon
 const customIcon = new L.Icon({
@@ -17,6 +18,7 @@ const customIcon = new L.Icon({
 });
 
 function MapPage() {
+  const [endPoint, setEndPoint] = useState('');
   const currentLocation = useCurrentLocation();
   const apiKey = process.env.REACT_APP_API_URL;
 
@@ -34,8 +36,25 @@ function MapPage() {
     return <div>Loading location data...</div>;
   }
 
+  const handleSearch = () => {
+    console.log("End Point:", endPoint);
+    // Add logic here to handle map actions based on the end point
+  };
+
   return (
     <div>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter end point"
+          value={endPoint}
+          onChange={e => setEndPoint(e.target.value)}
+          className="end-input"
+        />
+        <button onClick={handleSearch} className="search-button">
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
       <MapContainer center={[currentLocation.latitude, currentLocation.longitude]} zoom={13} style={{ height: "95vh", width: "100%" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={[currentLocation.latitude, currentLocation.longitude]} icon={customIcon} />
